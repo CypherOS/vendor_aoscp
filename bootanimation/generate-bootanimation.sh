2fc38391774a2e7c5560f324d04e1a6a0d3e53d8
Aaron Nixon <aarondoom@cookiedoom.com>
2017-04-27 16:15:09 -0700

    Cleanup this boot animation script, tonne of useless crap in it, plus convert allows maxspec on the command line
    Change-Id: I04dcda49256dbf9361023dc1bada29bb29eafe88

--- bootanimation/generate-bootanimation.sh
+++ bootanimation/generate-bootanimation.sh
@@ -12,58 +12,29 @@ FPS="60"
 
 OUT="$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION"
 
-#Set IMAGEWIDTH as the longer of the measurements
-if [ "$HEIGHT" -lt "$WIDTH" ]
-then
-    IMAGEWIDTH="$HEIGHT"
-else
-    IMAGEWIDTH="$WIDTH"
-fi
-
-#Set ORIGINAL_IMAGEWIDTH as the longer of the measurements
-if [ "$ORIGINAL_HEIGHT" -lt "$ORIGINAL_WIDTH" ]
-then
-    ORIGINAL_IMAGEWIDTH="$ORIGINAL_HEIGHT"
-    ORIGINAL_IMAGEHEIGHT="$ORIGINAL_WIDTH"
-else
-    ORIGINAL_IMAGEWIDTH="$ORIGINAL_WIDTH"
-    ORIGINAL_IMAGEHEIGHT="$ORIGINAL_HEIGHT"
-fi
-
-#Calculate aspect ratio of original image
-ASPECTRATIO=$(("$ORIGINAL_IMAGEWIDTH" / "$ORIGINAL_IMAGEHEIGHT"))
-
-#Check if HALF_RES
-if [ "$HALF_RES" = "true" ]
-then
-    IMAGEWIDTH=$(("$IMAGEWIDTH" / 2))
-fi
-
-#Set IMAGEHEIGHT
-IMAGEHEIGHT=$(("$IMAGEWIDTH" / "$ASPECTRATIO"))
-
 #Set image size
-RESOLUTION="${IMAGEWIDTH}x$IMAGEHEIGHT"
+RESOLUTION="${WIDTH}^x${HEIGHT}^>"
 
 #Create a folder for each part
 PARTS=$(echo "$PARTS" | tr " " "\n")
+
+rm -rf "$OUT/bootanimation/"
 
 for PART in $PARTS
 do
     mkdir -p "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part$PART"
 done
 
-#Decompress bootanimation.zip
+# Decompress bootanimation.zip
 unzip "vendor/aoscp/bootanimation/bootanimation.zip" -d "$OUT/bootanimation/"
 
-#Resize all images
-convert -resize "$RESOLUTION" -colors 250 "$OUT/bootanimation/"*"/"*".png"
+# Resize all images
+find $OUT/bootanimation/ -type f -name '*.png' -exec convert -resize "$RESOLUTION" -colors 250 {}
 
 # Create desc.txt
 echo "$IMAGEWIDTH $IMAGEHEIGHT" "$FPS" > "$OUT/bootanimation/desc.txt"
 cat "vendor/aoscp/bootanimation/desc.txt" >> "$OUT/bootanimation/desc.txt"
 
 # Create bootanimation.zip
-cd "$OUT/bootanimation"
-zip -0 "$OUT/bootanimation.zip" .
-
+cd "${OUT}/bootanimation"
+find -type f | sort | zip -@ -0 "${OUT}/bootanimation.zip"

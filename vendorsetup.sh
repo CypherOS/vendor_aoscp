8700d4ce969e3d668700dbfc1f5ffb164a00a56e
Mati Halperin <matias.halperin@gmail.com>
2017-05-03 13:12:26 -0400

    Updated method for adding lunch combos
    The result is the same, but the method used was quite outdated
    
    Change-Id: Id6101248dbbabe2c6462c736eb45825d3aa70f72

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,6 @@
-for device in $(cat vendor/aoscp/aoscp.devices)
+#!/bin/bash
+
+while read -r device || [[ -n $device ]]
 do
-add_lunch_combo aoscp_$device-userdebug
-done
+    add_lunch_combo "aoscp_$device-userdebug"
+done < vendor/aoscp/aoscp.devices

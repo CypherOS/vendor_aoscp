d553c021d1e991c45629a91c254edf5a4aa2df04
Chris <chris@cypheros.co>
2018-03-28 21:31:12 -0400

    themes: Support Black & Dark theme
    Themes are now handled in vendor instead of base, with the other accent
    overlays. Package name is updated to co.aoscp.theme.
    
    isAccent boolean has been removed from accents as this is deprecated for
    now in parfait. We can single out accents based on it's package prefix
    
    Change-Id: Iaf95882e9674d15dcd31078f229bc802a5b401fa

--- configs/common_theme_packages.mk
+++ configs/common_theme_packages.mk
@@ -1,41 +1,51 @@
 # Theme vendor extension
 -include vendor/aoscp/themes/Android.mk
 
+# Dark Theme
+PRODUCT_PACKAGES += \
+    SystemDarkTheme \
+    SettingsDarkTheme
+
+# Black Theme
+PRODUCT_PACKAGES += \
+    SystemBlackTheme \
+    SettingsBlackTheme
+
 # Deep Purple
 PRODUCT_PACKAGES += \
     DeepPurpleTheme
-	
+
 # Indigo
 PRODUCT_PACKAGES += \
     IndigoTheme
-	
+
 # Pink
 PRODUCT_PACKAGES += \
     PinkTheme
-	
+
 # Purple
 PRODUCT_PACKAGES += \
     PurpleTheme
-	
+
 # Red
 PRODUCT_PACKAGES += \
     RedTheme
-	
+
 # Sky Blue
 PRODUCT_PACKAGES += \
     SkyBlueTheme
-	
+
 # Yellow
 PRODUCT_PACKAGES += \
     YellowTheme
-	
+
 # Teal
 PRODUCT_PACKAGES += \
     TealTheme
-	
+
 # White
 PRODUCT_PACKAGES += \
     WhiteTheme
-	
+
 # Set Pixel blue light theme on Gboard
 PRODUCT_PROPERTY_OVERRIDES += ro.com.google.ime.theme_id=5

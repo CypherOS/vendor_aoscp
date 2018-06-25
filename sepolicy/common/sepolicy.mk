d29d56ac03fc00fb089295381d2f24440cf8979d
Chris <chris@cypheros.co>
2018-06-25 15:48:09 -0400

    sepolicy: Include vendor specific sepolicy
    Change-Id: Iaf516f7d28ec45f9338e810d8d6c754a3b7abb03

--- sepolicy/common/sepolicy.mk
+++ sepolicy/common/sepolicy.mk
@@ -8,3 +8,6 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     vendor/aoscp/sepolicy/common/private
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/aoscp/sepolicy/common/vendor

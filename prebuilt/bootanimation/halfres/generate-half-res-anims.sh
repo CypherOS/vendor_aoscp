656cf03e39119b4342bff05298e8dfa5bdb10566
Aaron Nixon <aarondoom@cookiedoom.com>
2018-07-10 13:33:55 -0700

    Half-res boot animations
    Change-Id: Ib4b43de3e7f6bbbddf33ccf7332be60cd9c6ab2e

--- prebuilt/bootanimation/halfres/generate-half-res-anims.sh
+++ prebuilt/bootanimation/halfres/generate-half-res-anims.sh
@@ -3,24 +3,55 @@
 HALF_RES_RESOLUTIONS="480 540 720 768 1080 1440"
 
 for i in $HALF_RES_RESOLUTIONS; do
-    rm -f $i.zip
-    mkdir $i
-    cd $i
-    if [ -f ../../$(($i/2)).zip ]; then
+    rm -f $i.zip;
+    mkdir $i;
+    cd $i;
+    half=$(($i/2));
+    if [ -f ../../$half.zip ]; then
 	# use the existing scaled images
-	echo "Using existing half-scale images instead of scaling from $i px"
-	unzip ../../$(($i/2)).zip
-	rm -f desc.txt
-	unzip ../../$i.zip desc.txt
+	echo "Using existing half-scale images instead of scaling from $i px";
+	unzip -q ../../$(($i/2)).zip;
+	rm -f desc.txt;
+	unzip -q ../../$i.zip desc.txt;
+        Trims=`find -type f -name 'trim.txt' | sort`
+        if [ Trims ]; then
+            for Trim in $Trims; do
+                rm $Trim;
+                unzip -q ../../$i.zip ${Trim:2};
+            done;
+        fi;
     else
-	unzip ../../$i.zip
+        # generate scaled images
+	echo "Scaling from $i px";
+	unzip -q ../../$i.zip;
 	for j in */*.[pP][nN][gG]; do
-	    convert $j -resize 50% tmp.png
-            pngnq -f -e.png tmp.png
-	    mv tmp.png $j
+	    convert $j -resize 50% tmp.png;
+            pngnq -f -e.png tmp.png;
+	    mv tmp.png $j;
 	done
     fi
-    find -type f | sort | zip -@ -0 ../$i.zip .
+    Trims=`find -type f -name 'trim.txt' | sort`
+    if [ Trims ]; then
+        for Trim in $Trims; do
+            while read -r line; do
+                Dim=`expr match "$line" '[0-9]*x[0-9]*'`;
+                X=`expr index "$line" 'x'`;
+                W=$((${line:0:$(($X-1))}/2));
+                H=$((${line:$X:$(($Dim-$X))}/2));
+                Off=${line:$(($Dim+1))}
+                Mid=`expr index "$Off" '\+'`;
+                OffX=${Off:0:$(($Mid-1))};
+                OffY=${Off:$Mid:$((${#Off}-$Mid-1))}; # Trim the carriage return off the string
+                HW=$(($W/2));
+                HH=$(($H/2));
+                L=$(($OffX + $HW));
+                T=$(($OffY + $HH));
+                echo "${W}x${H}+${L}+${T}" >> $Trim.new;
+            done < $Trim
+            mv $Trim.new $Trim
+        done
+    fi
+    find -type f | sort | zip -q -@ -0 ../$i.zip .
     cd ..
     rm -rf $i
 done

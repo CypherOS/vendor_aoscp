fd16534f32139999a8ac73e9c1f76c38f6ae852b
Chris <chris@cypheros.co>
2018-07-17 17:45:46 -0400

    Version bump maintenance patch to 2018-07-17
    Change-Id: Idc65c4f10e5c15bb43075eed56f6864da93a18a4

--- configs/common.mk
+++ configs/common.mk
@@ -4,7 +4,7 @@ PRODUCT_BRAND ?= aoscp
 export AOSCP_VERSION := 6.0.0
 export AOSCP_CODENAME := Parfait
 export AOSCP_BUILD_NUMBER := CBNP.8102.$(shell date -u +%d).$(shell date -u +%m)052
-export AOSCP_MAINTENANCE_PATCH := 2018-07-12
+export AOSCP_MAINTENANCE_PATCH := 2018-07-17
 
 AOSCP_DISPLAY_VERSION := $(AOSCP_VERSION)
 

641539b5b6a16fc075d72376135d604a985f8a90
Chris <chris@cypheros.co>
2018-08-20 05:36:53 -0400

    configs: Update explicit font path variable
    LOCAL_PATH is already defined in common makefile, restructure the font
    path
    
    Change-Id: I1acce7b2a1a021d7f987789becf48339c4dcf910

--- configs/fonts.mk
+++ configs/fonts.mk
@@ -1,10 +1,10 @@
-LOCAL_PATH := vendor/aoscp/prebuilt/common/
+FONT_PATH := $(LOCAL_PATH)prebuilt/common/fonts/
 
 # GoogleSans
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
-    $(LOCAL_PATH)fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
-    $(LOCAL_PATH)fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
-    $(LOCAL_PATH)fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
-    $(LOCAL_PATH)fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
-    $(LOCAL_PATH)fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+    $(FONT_PATH)GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    $(FONT_PATH)GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    $(FONT_PATH)GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    $(FONT_PATH)GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    $(FONT_PATH)GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    $(FONT_PATH)GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf

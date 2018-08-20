bf5c199049561338d625e781c52a5cd1a55c2997
Chris <chris@cypheros.co>
2018-08-20 05:56:44 -0400

    themes: Guard theme makefiles under Luna platform
    The build system will forcefully include this makefile, let's make sure
    it's guarded by the luna platform as well
    
    Change-Id: If01a82758bb592d5db9aae85b9150cc3a7ba955d

--- themes/Android.mk
+++ themes/Android.mk
@@ -24,5 +24,6 @@ LOCAL_PATH := $(call my-dir)
 
 # if some modules are built directly from this directory (not subdirectories),
 # their rules should be written here.
-
+ifeq ($(TARGET_LUNA_PLATFORM_THEMES),true)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+endif

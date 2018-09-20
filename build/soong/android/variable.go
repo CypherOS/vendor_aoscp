7afe988eeffea9d4240515f6783a4c3e1b81db9b
Paul Keith <javelinanddart@gmail.com>
2018-09-21 00:50:16 +0200

    Revert "soong_config: Add TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS"
    * The per-process API level override will take care of this
    
    This reverts commit cf7f7d48d0297f9a64413076d755b00e10e48c9a.
    
    Change-Id: I3c0b5879ddf8e9dcc0b56a6e9a2855e515685389

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -6,9 +6,6 @@ type Product_variables struct {
 	Has_legacy_camera_hal1 struct {
 		Cflags []string
 	}
-	Needs_text_relocations struct {
-		Cppflags []string
-	}
 	Target_process_sdk_version_override struct {
 		Cppflags []string
 	}
@@ -33,7 +30,6 @@ type ProductVariables struct {
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Target_process_sdk_version_override *string `json:",omitempty"`
-	Needs_text_relocations  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`

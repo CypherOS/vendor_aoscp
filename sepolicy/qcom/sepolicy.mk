2db3b0d1d8e9093e77cebf26abaa407606d2b0a2
Chris Crump <chris@cypheros.co>
2019-05-25 05:16:40 -0400

    sepolicy: vendor/aoscp -> device/aoscp for sepolicy
    Change-Id: I77995152c40b69a9209ae244622b031a1ab32906

--- sepolicy/qcom/sepolicy.mk
+++
@@ -1,8 +0,0 @@
-#
-# This policy configuration will be used by all qcom products
-# that inherit from aoscp
-#
-
-BOARD_SEPOLICY_DIRS += \
-    vendor/aoscp/sepolicy/qcom/common \
-    vendor/aoscp/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)

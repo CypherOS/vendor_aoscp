44216b2a3ae4e92edbb4ecf723447cef298945fe
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-06-07 09:43:11 -0400

    build: Never allow neverallows
    Change-Id: Ibf07e9e319d676666e3e7389720c24917e38a9ac

--- build/core/config.mk
+++ build/core/config.mk
@@ -17,11 +17,6 @@
 BUILD_MAVEN_PREBUILT := $(TOP)/vendor/aoscp/build/core/maven_artifact.mk
 PUBLISH_MAVEN_PREBUILT := $(TOP)/vendor/aoscp/build/core/maven_artifact_publish.mk
 
-# We modify several neverallows, so let the build proceed
-ifneq ($(TARGET_BUILD_VARIANT),user)
-SELINUX_IGNORE_NEVERALLOWS := true
-endif
-
 # Rules for MTK targets
 include $(TOPDIR)vendor/aoscp/build/core/mtk_target.mk
 
